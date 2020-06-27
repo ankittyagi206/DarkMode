485782553a0a5063fea6bad226ff2c044ffcf8f3
ankit206 <tyagiankit370@gmail.com>
2020-06-28 04:31:41 +0530

    initialCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const getMode =()=>{
+
+    return JSON.parse(localStorage.getItem("mode")) || false
+  }
+  const[dark,setDark]=useState(getMode);
+
+  useEffect(()=>{
+localStorage.setItem("mode",JSON.stringify(dark))
+
+  },[dark])
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={dark ? "App dark-mode":"App"}>
+        <div className="nav">
+              <label className="switch">
+                      <input type="checkbox"
+                      checked={dark}
+                      onChange={()=>setDark(!dark)}/>
+                      <span className="slider round"></span>
+              </label>
+        </div>
+
+        <div className="content">
+            <h1>{dark?"Dark Mode is On":"Light Mode is On "}</h1>
+            <p style={{fontsize:"20px"}}>{dark?"You are in the Dark Mode" :"For Dark Mode press Togge"}</p>
+    </div>
     </div>
   );
 }
